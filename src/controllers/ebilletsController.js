b2dafda066917891f02178e454020a6bd3b27295
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-09-24 15:51:40 +0300

    test 19

--- src/controllers/ebilletsController.js
+++ src/controllers/ebilletsController.js
@@ -1,4 +1,4 @@
-//import { tri_reportByASC_ref } from '../utils/crud/function.js';
+import { tri_reportByASC_ref } from '../utils/crud/function.js';
 //import { compressed_obj, decompressed_obj } from '../utils/compression/compression.js'
 
 let Ebilletss = []; // Mémoire (tu peux remplacer par DB ou fichier)
@@ -8,10 +8,10 @@ export function getEbillets(req, res) {
 }
 
 export function tri_reportByASC_ref_Ebillets(req, res) {
-  const { data } = req.body;
+  const { data } = (req.body);
   if (!data) return res.status(400).json({ error: "données requis" });
     
   //const newEbillets = { id: Date.now(), text };
   //Ebilletss.push(newEbillets);
-  res.status(201).json(((data)));
+  res.status(201).json((tri_reportByASC_ref(data)));
 }
