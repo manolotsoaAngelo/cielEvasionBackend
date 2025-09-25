46275f55c38914ddef06d4f4811c26e31584448f
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-09-25 09:30:09 +0300

    test 27

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import ebilletsRoutes from "./src/routes/ebillets.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
   res.send("404 not found");
