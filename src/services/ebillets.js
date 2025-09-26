ed6816f0cccbd17f756acf9518d98c3d1a1684e9
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-09-26 10:35:15 +0300

    tets

--- src/services/ebillets.js
+++ src/services/ebillets.js
@@ -6,5 +6,5 @@ export async function all_ebillet() {
             "Content-Type": "application/json"
         }
     });
-    return decompressed_obj(await response.json());
+    return (await response.json());
 }
