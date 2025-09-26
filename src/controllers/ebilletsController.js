074d849577c22acdcad669446ab1e15c8362210f
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-09-26 10:01:07 +0300

    test 30

--- src/controllers/ebilletsController.js
+++ src/controllers/ebilletsController.js
@@ -1,11 +1,9 @@
 import { tri_reportByASC_ref } from '../utils/crud/function.js';
 import { compressed_obj, decompressed_obj } from '../utils/compression/compression.js'
 
-let Ebilletss = []; // Mémoire (tu peux remplacer par DB ou fichier)
-
 export async function getEbillets(req, res) {
-    const response = await fetch("https://ciel-evasion.fr/_functions/myFunction/findMe", {
-        method: "GET", // ou "POST" selon ton endpoint
+    const response = await fetch("https://ciel-evasion.fr/_functions/myFunction/all_ebillet", {
+        method: "GET",
         headers: {
             "Content-Type": "application/json"
         }
