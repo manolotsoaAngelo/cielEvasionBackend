18a7d1ffc96a5c3ac51775ca12a1c82eaa1fff0c
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-09-26 10:39:55 +0300

    test 30

--- src/controllers/ebilletsController.js
+++ src/controllers/ebilletsController.js
@@ -1,9 +1,16 @@
 import { tri_reportByASC_ref } from '../utils/crud/function.js';
 import { compressed_obj, decompressed_obj } from '../utils/compression/compression.js'
-import { all_ebillet } from '../services/ebillets.js';
+//import { all_ebillet } from '../services/ebillets.js';
 
 export async function getEbillets(req, res) {
-    res.json(all_ebillet());
+    const response = await fetch("https://ciel-evasion.fr/_functions/myFunction/all_ebillet", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    //return (await response.json());
+    res.json(await response.json());
 }
 
 export async function tri_reportByASC_ref_Ebillets(req, res) {
