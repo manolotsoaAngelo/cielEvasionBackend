ce2d403842aa6dae55f996a07b0416da86bd0325
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-10-28 07:22:27 +0300

    test 116

--- src/controllers/functionController.js
+++ src/controllers/functionController.js
@@ -1,12 +1,18 @@
-import { tri_reportByASC_ref } from '../utils/crud/function.js';
-import { compressed_obj, decompressed_obj } from '../utils/compression/compression.js'
-import { crypter, decrypter } from '../utils/cryptographie/cryptographie.js'
+import { tri_reportByASC_ref } from "../utils/crud/function.js";
 import {
-    chaine_opt_partenaire,
-    chaine_opt,
-    opt_reportByRef
-} from '../utils/tinyFunction/tinyFunction.js';
-import { init_cachedData_ebillet } from '../utils/fullData/ebillets.js';
+  compressed_obj,
+  decompressed_obj,
+} from "../utils/compression/compression.js";
+import { crypter, decrypter } from "../utils/cryptographie/cryptographie.js";
+import {
+  chaine_opt_partenaire,
+  chaine_opt,
+  opt_reportByRef
+} from "../utils/tinyFunction/tinyFunction.js";
+import { init_cachedData_ebillet } from "../utils/fullData/ebillets.js";
+import { init_cachedData_orders } from "../utils/fullData/ebillets.js";
+import { init_cachedData_partenaire } from "../utils/fullData/ebillets.js";
+import { init_cachedData_users } from "../utils/fullData/ebillets.js";
 
 import OrdersService from "../services/orders.js";
 
@@ -15,45 +21,52 @@ import OrdersService from "../services/orders.js";
 ///import { services_post, services_get } from 'backend/modules/server/server'
 ///return await services_post("L2FwaS9mdW5jdGlvbi9ydW5GdW5jdGlvbg==", { typeFunction: 'opt_reportByRef', valeur: ref })
 
+function init_cachedData(valeur) {
+  init_cachedData_ebillet(valeur);
+  init_cachedData_orders(valeur);
+  init_cachedData_partenaire(valeur);
+  init_cachedData_users(valeur);
+}
+
 export async function runFunction(req, res) {
-    const { data } = (req.body);
-    if (!data) return res.status(400).json({ error: "données requis" });
-    let value = decompressed_obj(data)
-    let result
-    switch (value.typeFunction) {
-        case 'init_cachedData_ebillet':
-            result = init_cachedData_ebillet(value.valeur)
-            break;
-        ///Orders
-        case 'create_order_new':
-            result = await OrdersService.create(value.valeur)
-            break;
-        case 'tri_reportByASC_ref':
-            result = await tri_reportByASC_ref(value.valeur)
-            break;
-        case 'chaine_opt':
-            result = await chaine_opt(value.valeur)
-            break;
-        case 'opt_reportByRef':
-            result = await opt_reportByRef(value.valeur)
-            break;
-        case 'chaine_opt_partenaire':
-            result = await chaine_opt_partenaire(value.valeur)
-            break;
-        case 'crypter':
-            result = crypter(value.valeur)
-            break;
-        case 'decrypter':
-            result = decrypter(value.valeur)
-            break;
-        case 'compressed_obj':
-            result = compressed_obj(value.valeur)
-            break;
-        case 'decompressed_obj':
-            result = decompressed_obj(value.valeur)
-            break;
-        default:
-            break;
-    }
-    res.status(201).json(compressed_obj(result));
+  const { data } = req.body;
+  if (!data) return res.status(400).json({ error: "données requis" });
+  let value = decompressed_obj(data);
+  let result;
+  switch (value.typeFunction) {
+    case "init_cachedData":
+      result = init_cachedData(value.valeur);
+      break;
+    ///Orders
+    case "create_order_new":
+      result = await OrdersService.create(value.valeur);
+      break;
+    case "tri_reportByASC_ref":
+      result = await tri_reportByASC_ref(value.valeur);
+      break;
+    case "chaine_opt":
+      result = await chaine_opt(value.valeur);
+      break;
+    case "opt_reportByRef":
+      result = await opt_reportByRef(value.valeur);
+      break;
+    case "chaine_opt_partenaire":
+      result = await chaine_opt_partenaire(value.valeur);
+      break;
+    case "crypter":
+      result = crypter(value.valeur);
+      break;
+    case "decrypter":
+      result = decrypter(value.valeur);
+      break;
+    case "compressed_obj":
+      result = compressed_obj(value.valeur);
+      break;
+    case "decompressed_obj":
+      result = decompressed_obj(value.valeur);
+      break;
+    default:
+      break;
+  }
+  res.status(201).json(compressed_obj(result));
 }
--- src/utils/fullData/orders.js
+++ src/utils/fullData/orders.js
@@ -5,6 +5,10 @@ let lastFetchTime = 0;
 let refreshPromise = null;
 const CACHE_DURATION = 5 * 60 * 1000;
 
+export function init_cachedData_orders(valeur) {
+    cachedData = valeur
+}
+
 export async function FullData(wixData_url_get_FullData) {
     const now = Date.now();
     const hasCache = cachedData && (now - lastFetchTime) < CACHE_DURATION * 2;
