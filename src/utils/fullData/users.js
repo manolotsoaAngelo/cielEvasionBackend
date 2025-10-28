8d0523649f931ccc8d4055b171cb93ccb457f54f
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-10-28 11:02:56 +0300

    test 123

--- src/utils/fullData/users.js
+++ src/utils/fullData/users.js
@@ -6,7 +6,10 @@ let refreshPromise = null;
 const CACHE_DURATION = 5 * 60 * 1000;
 
 export function init_cachedData_users(valeur) {
-   return cachedData = valeur
+    cachedData = null;
+ lastFetchTime = 0;
+ refreshPromise = null;
+   return  valeur
 }
 
 export async function FullData(wixData_url_get_FullData) {
