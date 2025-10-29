8c084b35d22c58ddb9084b41d0a2950713a87ae2
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-10-29 04:13:44 +0300

    test 147

--- src/utils/compression/compression.js
+++ src/utils/compression/compression.js
@@ -8,3 +8,23 @@ export function compressed_obj(data) {
 export function decompressed_obj(compressed_obj) {
     return JSON.parse(LZString.decompressFromUTF16(compressed_obj));
 }
+
+/*
+
+import { services_post, services_get } from 'backend/modules/server/server'
+//import { compressed_obj, decompressed_obj } from 'backend/modules/fonctionnalites/information/compression'
+
+export async function compressed_obj(data) {
+    if (data) {
+        return await services_post("L2FwaS9mdW5jdGlvbi9ydW5GdW5jdGlvbg==", { typeFunction: "compressed_obj", valeur: data })
+    }
+}
+
+export async function decompressed_obj(data) {
+    if (data) {
+        return await services_post("L2FwaS9mdW5jdGlvbi9ydW5GdW5jdGlvbg==", { typeFunction: "decompressed_obj", valeur: data })
+    }
+}
+
+
+*/
