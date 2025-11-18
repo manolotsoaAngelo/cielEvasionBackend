3968a2e003c139a2d9a8a6f50911a70e34582a47
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-11-18 09:20:24 +0300

    test 304

--- src/services/css.js
+++ src/services/css.js
@@ -15,101 +15,48 @@ class CssService {
     //"https://ciel-evasion.fr/_functions/WixCss/get_css_partenaires_header_footer_body_fix"
     ///<link rel="stylesheet" href="https://ciel-evasion.fr/_functions/WixCss/get_css_partenaires_header_footer_body_fix">
 
-    let css = `
-@media (min-width: 1025px) {
-  #comp-mdiw6s9o,
-  #comp-m59ic5d3,
-  #comp-m56uvovw,
-  #comp-mdzn7y3o {
-    position: fixed !important;
-    inset: 0 auto auto 0;
-    width: 100%;
-    z-index: 10000;
-    background: inherit;
-    transform: translateZ(0);
-  }
-
-  #comp-mdiw7w5g2,
-  #comp-m59ieqh9,
-  #comp-m59ob8a72,
-  #comp-mdznowf0 {
-    position: fixed !important;
-    inset: auto auto 0 0;
-    width: 100%;
-    z-index: 10000;
-    background: inherit;
-    transform: translateZ(0);
-  }
-
-  #comp-mceqgvd0,
-  #comp-m59icq1u,
-  #comp-m56rqvpq,
-  #comp-mdznalu2 {
-    padding-top: 120px;
-    padding-bottom: 25px;
-    box-sizing: border-box;
-  }
-}
-
-@media (max-width: 1024px) {
-  #comp-mdiw6s9o,
-  #comp-m59ic5d3,
-  #comp-m56uvovw,#comp-mdiw6s9o,
+    let css = `/* HEADER FIXE */
+#comp-mdiw6s9o,
 #comp-m59ic5d3,
 #comp-m56uvovw,
 #comp-mdzn7y3o {
   position: fixed !important;
-  inset: 0 auto auto 0;
+  top: 0;
+  left: 0;
   width: 100%;
   z-index: 10000;
   background: inherit;
-  transform: translateZ(0);
 }
 
+/* FOOTER FIXE */
 #comp-mdiw7w5g2,
 #comp-m59ieqh9,
 #comp-m59ob8a72,
 #comp-mdznowf0 {
   position: fixed !important;
-  inset: auto auto 0 0;
+  bottom: 0;
+  left: 0;
   width: 100%;
   z-index: 10000;
   background: inherit;
-  transform: translateZ(0);
 }
 
+/* CONTENU */
 #comp-mceqgvd0,
 #comp-m59icq1u,
 #comp-m56rqvpq,
 #comp-mdznalu2 {
-  padding-top: 120px;
-  padding-bottom: 25px;
   box-sizing: border-box;
-}
-
-@media (max-width: 1024px) {
-  #comp-mceqgvd0,
-  #comp-m59icq1u,
-  #comp-m56rqvpq,
-  #comp-mdznalu2 {
-    padding-top: 90px;
-    padding-bottom: 20px;
-  }
-}
-
-@media (max-width: 640px) {
-  #comp-mceqgvd0,
-  #comp-m59icq1u,
-  #comp-m56rqvpq,
-  #comp-mdznalu2 {
-    padding-top: 70px;
-    padding-bottom: 18px;
-  }
 }
 
 html {
   scroll-behavior: smooth;
 }
+
+body {
+  margin: 0;
+}
+
 `
     return css
   }
