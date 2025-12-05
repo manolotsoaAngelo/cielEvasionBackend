c1fe253d3e4874f43a40435528f3da911808a73d
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2025-12-05 16:32:29 +0300

    test 231

--- src/controllers/ebilletsController.js
+++ src/controllers/ebilletsController.js
@@ -13,9 +13,8 @@ import EbilletsService from "../services/ebillets.js";
 //console.log(await EbilletsService.getAllEbilletFactureByPartenaire("15d9204a-b1d1-4288-8e5e-24e0fde1b8d3"))
 
 export async function get_All_Ebillet_Facture(req, res) {
-  let value = post(req, res);
-  let result = await EbilletsService.getAllEbilletFacture();
-  res.status(201).json(compressed_obj(result));
+  let all_ebillet_data = await EbilletsService.getAllEbilletFacture();
+  res.json(compressed_obj(all_ebillet_data));
 }
 
 export async function get_All_Ebillet_Facture_ByPartenaire(req, res) {
