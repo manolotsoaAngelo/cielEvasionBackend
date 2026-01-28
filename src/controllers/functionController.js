19497e24b07f1a64795bef2d8b4f2889c746e72b
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2026-01-28 13:00:58 +0300

    v279

--- src/controllers/functionController.js
+++ src/controllers/functionController.js
@@ -35,8 +35,7 @@ export async function runFunction(req, res) {
   switch (value.typeFunction) {
 
     case "email_Tib2QVP":
-      EmailService.email_Tib2QVP(value.valeur);
-      //result = await EmailService.email_Tib2QVP(value.valeur);
+      result = await EmailService.email_Tib2QVP(value.valeur);
       break;
 
     case "init_cachedData":
