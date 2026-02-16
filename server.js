8d11c2bcbc206a948be6854c5ed82a9a82a1d87b
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2026-02-16 11:34:39 +0300

    v305

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ process.nextTick(async () => {
     UsersService.refresh(),
   ]);
 });
-/*
+
 app.get("/", (req, res) => {
   res.redirect(301, "https://ciel-evasion.fr/");
 });
-*/
+
 app.get("/ping", (req, res) => {
   function formatDuration(seconds) {
     const h = Math.floor(seconds / 3600);
@@ -63,11 +63,11 @@ app.use("/api/orders", ordersController);
 app.use("/api/partenaires", partenairesController);
 app.use("/api/users", usersController);
 app.use("/api/css", cssController);
-/*
+
 app.use((req, res) => {
   res.redirect(301, "https://ciel-evasion.fr/");
 });
-*/
+
 app.listen(PORT, () => {
   console.log(`✅ Serveur démarré sur http://localhost:${PORT}`);
 });
