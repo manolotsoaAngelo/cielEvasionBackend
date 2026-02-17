9047ae3098d4199ab9f3b7dfb4af65fda127af94
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2026-02-17 11:27:00 +0300

    v309

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { get_all_order } from "../controllers/ordersController.js";
+import { get_all_order, post_nom_prenom_byOrder } from "../controllers/ordersController.js";
 
 const router = express.Router();
 
