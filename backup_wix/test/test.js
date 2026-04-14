3915b35fcc3dad70504a390d8c2bbc17b0ebaf0b
Manolotsoa Angelo <manolotsoa.randriambeloniaina@gmail.com>
2026-04-14 08:40:29 +0300

    v372

--- backup_wix/test/test.js
+++
@@ -1,9 +0,0 @@
-import OrdersService from "../../src/services/orders.js";
-import AiService from "../../src/services/ai.js";
-
-export async function test() {
-  let num = 11538
- console.log(await OrdersService.get_nom_prenom_byOrder(num)) 
-}
-
-test()
